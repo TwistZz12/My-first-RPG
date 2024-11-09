3a35beefca8922962a57159dda02825de0cf3762
TwistZz12 <twistzzmeng@gmail.com>
2024-11-09 16:32:06 +0000

    Fix wall-sticking issues, add dash and animations.

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,14 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
-    [SerializeField]private float xSpeed;
-    [SerializeField]private float ySpeed;
+
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
+    [SerializeField] private float dashSpeed;
+
+    [Header("Dash info")]
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
     private float xInput;
+
+
     private int facingDir = 1;
     private bool facingRight = true;
 
@@ -34,6 +43,15 @@ public class NewBehaviourScript : MonoBehaviour
 
         CollisionChecks();
 
+
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;//从0自减
+
+
+
+
+
         AnimatorControllers();
 
         FlipController();
@@ -52,17 +70,39 @@ public class NewBehaviourScript : MonoBehaviour
         {
             Jump();
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))//触发条件赋值自减
+        {
+            DashAbility();
+        }
+
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0)
+        {
+            dashTime = dashDuration;
+            dashCooldown = dashCooldownTimer;
+        }
     }
 
     private void Movement()
     {
-        rb.velocity = new Vector2(xInput * xSpeed, rb.velocity.y);
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(xInput * dashSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * xSpeed, rb.velocity.y);
+        }
     }
 
     private void Jump()
     {
-        if(isGrounded)
-        rb.velocity = new Vector2(rb.velocity.x, ySpeed);
+        if (isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, ySpeed);
     }
 
     private void AnimatorControllers()
@@ -71,8 +111,9 @@ public class NewBehaviourScript : MonoBehaviour
 
         anim.SetFloat("yVelocity", rb.velocity.y);
 
-        anim.SetBool("isMoving",isMoving);
+        anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
 
     }
     private void Flip()
@@ -84,13 +125,13 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void FlipController()
     {
-        if (rb.velocity.x > 0 && !facingRight)      
+        if (rb.velocity.x > 0 && !facingRight)
             Flip();
         else if (rb.velocity.x < 0 && facingRight)
-            Flip();      
-     }
+            Flip();
+    }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position , new Vector3(transform.position.x,transform.position.y - groundCheckDistance));//可视化向下延伸的线条，调整跳跃触碰层
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));//可视化向下延伸的线条，调整跳跃触碰层
     }
 }
